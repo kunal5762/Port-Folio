bc5363ff52e08190d29672ffffee1f2a5eddd0ba
kunal rana <kunalrana5762@gmail.com>
2025-12-26 20:57:28 +0530

    updated UI and connent form to gmail through emailjs

--- src/components/Navitmes/Contact.jsx
+++ src/components/Navitmes/Contact.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import img12 from '../../../src/assets/ContactUs.png'
 import { FaInstagram } from "react-icons/fa";
+import emailjs, { send } from '@emailjs/browser';
+import { useRef } from 'react';
 function Contact() {
+  const formsubmission = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm("service_ws2ibug", "template_2l7d3z8", formsubmission.current, "h5CDv0hZ7v8hI8piL")
+    .then(() => {
+      alert('Message sent successfully');
+      formsubmission.current.reset();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  };
   return (
     <div className='flex justify-center items-center max-sm:pr-10'>
       <div className=' secondFadeDrop max-sm:pl-15 max-sm:w-[500px] max-sm:h-170 max-sm:pt-23 max-sm:flex max-sm:justify-center pt-12 flex justify-center items-center w-350 h-150 mb-10'>
@@ -11,22 +28,20 @@ function Contact() {
           </div>
           <div className='flex flex-col gap-2 h-100'>
             <h1 className='text-[50px]'>CONTACT US</h1>
-            <form className='flex flex-col gap-3'>
+            <form ref={formsubmission} onSubmit={sendEmail} className='flex flex-col gap-3'>
               <label>Full Name : </label>
-              <input className='border-b-2' type="text" />
-
+              <input className='border-b-2' type="text" name="name" required />
               <label>E-mail : </label>
-              <input className='border-b-2' type="email" />
-
+              <input className='border-b-2' type="email" name="email" required />
+              <input type="hidden" name="title" value="Portfolio Contact Message" />
               <label>Message : </label>
-              <textarea className='border-b-2'></textarea> <br />
-            </form>
-            <div className='flex gap-10 justify-center items-center'>
-              <button className='h-10 w-50 bg-white rounded-3xl text-[black]'>Contact Us</button>
-              <div className='flex gap-8 '>
+              <textarea className='border-b-2' name="message"
+                required></textarea> <br />
+              <div className='flex gap-10 justify-center items-center'>
+                <button type='submit' className='h-10 w-50 bg-white rounded-3xl text-[black]'>Contact Us</button>
                 <a className='text-[30px]' href="https://www.instagram.com/__rana__kunal__?igsh=MWVxamluOGhxcGN5Nw==" target='_blank' rel="noopener noreferrer"><FaInstagram /></a>
               </div>
-            </div>
+            </form>
           </div>
         </div >
       </div >
